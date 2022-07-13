6bc617192fb3f28423f74670463af7379467f2b2
charJKL <11600464+charJKL@users.noreply.github.com>
2022-07-13 14:25:31 +0200

    Init TowerDefenceGame.

---
+++ Assets/Scripts/TowerEnemiesInRange.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerEnemiesInRange : MonoBehaviour
+{
+	[SerializeField] private GameObject[] enemies;
+	
+	private SphereCollider range;
+	
+}
