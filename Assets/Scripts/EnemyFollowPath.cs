acd4ec65b03730b4aea12f7515598a00e429af9f
charJKL <11600464+charJKL@users.noreply.github.com>
2022-07-14 10:35:22 +0200

    Make enemy follow the curve path.

--- Assets/Scripts/EnemyFollowPath.cs
+++ Assets/Scripts/EnemyFollowPath.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
 
 public class EnemyFollowPath : MonoBehaviour
 {
 	[SerializeField] private float speed = 3f;
+	[SerializeField] private PathCreator path;
+	
+	private float distance;
+	
+	private void Start()
+	{
+		distance = 0;
+	}
 	
 	private void Update()
 	{
-		transform.position += transform.forward * speed * Time.deltaTime;
+		if(path == null) return;
+		distance += speed * Time.deltaTime;
+	
+		transform.position = path.path.GetPointAtDistance(distance, EndOfPathInstruction.Stop);
+		transform.rotation = path.path.GetRotationAtDistance(distance, EndOfPathInstruction.Stop);
 	}
 }
